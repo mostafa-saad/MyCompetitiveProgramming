79e9616cbd389db6910efd4ba650ef33c380843f
mostafa-saad <mostafa.saad.fci@gmail.com>
2017-08-15 20:48:22 -0700

    up

---
+++ UVA/UVA_10125.cpp
@@ -0,0 +1,5 @@
+One Solution: Meet in middle, other way based on brute force
+	See both here: http://blog.csdn.net/synapse7/article/details/15815645
+	
+Another meet in middle (smaller code)
+	https://github.com/thabetx/CompetitiveProgramming/blob/master/UVA/10125.cpp
